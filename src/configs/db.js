e35c8bd9e30c25580263bc106d81905bb80fe7de
Nitesh Kumar <kumarnitesh88441@gmail.com>
2024-05-31 00:40:07 +0530

    remove dotenv file

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,4 +1,4 @@
 module.exports={
     DB_NAME:"crm_db",
-    DB_URL:"mongodb+srv://developer:nitesh-developer@cluster0.monh9xn.mongodb.net/Knovator?retryWrites=true&w=majority&appName=Cluster0"
+    DB_URL:"mongodb+srv://{username}{password}@cluster0.monh9xn.mongodb.net/Knovator?retryWrites=true&w=majority&appName=Cluster0"
 }
