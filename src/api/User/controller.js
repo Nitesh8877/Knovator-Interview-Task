89b4f15332d375222f19dbdf41714f6e1afd011d
Nitesh Kumar <82534120+Nitesh8877@users.noreply.github.com>
2024-05-31 23:49:07 +0530

    Update controller.js

--- src/api/User/controller.js
+++ src/api/User/controller.js
@@ -7,13 +7,13 @@ module.exports={
     
 register: async (req, res) => {
     let { name, email, password } = req.body;
-    if(!name) {return badReq(res, "Name is required")}
-    if(!email) {return badReq(res, "Email is required")}
-    if(!password) {return badReq(res, "Password is required")}
+    if(!name) {return badReq(res,400, "Name is required")}
+    if(!email) {return badReq(res,400, "Email is required")}
+    if(!password) {return badReq(res,400, "Password is required")}
     try {
       let user = await getUserData({ email });
       if (user) {
-        return badReq(res,'Email already exists');
+        return badReq(res,409,'Email already exists');
       }
       const salt = await bcrypt.genSalt(10);
       password = await bcrypt.hash(password, salt);
@@ -31,16 +31,16 @@ register: async (req, res) => {
  login: async (req, res) => {
     let { email, password } = req.body;
    
-    if(!email) {return badReq(res, "Email is required")}
-    if(!password) {return badReq(res, "Password is required")}
+    if(!email) {return badReq(res,400, "Email is required")}
+    if(!password) {return badReq(res,400, "Password is required")}
     try {
       const user = await getUserData({ email });
       if (!user) {
-        return badReq(res, 'Invalid credentials');
+        return badReq(res,400, 'Invalid credentials');
       }
       const isMatch = await bcrypt.compare(password, user.password);
       if (!isMatch) {
-        return badReq('Invalid credentials');
+        return badReq(res,400, 'Invalid credentials');
       }
       const payload = { id: user.id };
       jwt.sign(
